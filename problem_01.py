57b0c5e4cecf62f95d3b42adeb0294af0cff9d3e
christian0418 <tanchristian_bsit@plmun.edu.ph>
2025-02-10 12:50:21 +0800

    Update problem_01.py

--- problem_01.py
+++ problem_01.py
@@ -8,6 +8,7 @@ def main():
     print("Simple Calculator Program")
 
     num1 = int(input("Enter 1st number: "))
+    
     num2 = int(input("Enter 2nd number: "))
 
     print(f"The sum is {num1 + num2}")
