4379081019ccf27742b6bfa34860414b3a214052
christian0418 <tanchristian_bsit@plmun.edu.ph>
2025-02-10 12:57:12 +0800

    Update problem_01.py

--- problem_01.py
+++ problem_01.py
@@ -8,13 +8,13 @@ def main():
     
     num1 = int(input("Enter 1st number: "))
     
-num2 = int(input("Enter 2nd number: "))
+    num2 = int(input("Enter 2nd number: "))
 
-print(f"The sum is {num1 + num2}")
+    print(f"The sum is {num1 + num2}")
 
-print(f"The difference is {num1 - num2}")
+    print(f"The difference is {num1 - num2}")
 
-print(f"The quotient is {(num1 / num2):.2f}")
+    print(f"The quotient is {(num1 / num2):.2f}")
 
 if __name__ == "__main__":
     main()
