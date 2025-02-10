99f32e615215bbc4c2a805a0da142f4b0b3944e2
christian0418 <tanchristian_bsit@plmun.edu.ph>
2025-02-10 12:37:42 +0800

    Update problem_01.py

--- problem_01.py
+++ problem_01.py
@@ -1,4 +1,4 @@
-# YOUR NAME
+# CHRISTIAN TAN
 # ITELEC2
 # Problem Set 01 - Problem 01
 # Simple Calculator Program
@@ -9,7 +9,6 @@ def main():
     print("Simple Calculator Program")
     
     num1 = int(input("Enter 1st number: "))
-    
     num2 = int(input("Enter 2nd number: "))
     
     print(f"The sum is {num1 + num2}")
