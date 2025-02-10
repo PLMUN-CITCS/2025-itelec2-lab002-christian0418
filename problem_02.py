cf4f1ce376a035aa64594785bd75df9dade11a49
christian0418 <tanchristian_bsit@plmun.edu.ph>
2025-02-10 12:21:04 +0800

    Update problem_02.py

--- problem_02.py
+++ problem_02.py
@@ -8,6 +8,5 @@ def main():
     print("Square the Number Program")
     number = int(input("Enter a number: "))
     print(f"The square of {number} is {(number * number):.2f}")
-
 if __name__ == "__main__":
     main()
