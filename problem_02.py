c558dc23e11cc101294ddfc75ed1aeede7a09a8a
christian0418 <tanchristian_bsit@plmun.edu.ph>
2025-02-10 12:45:19 +0800

    Update problem_02.py

--- problem_02.py
+++ problem_02.py
@@ -1,16 +1,14 @@
 # CHRISTIAN TAN
 # ITELEC2
 # Problem Set 01 - Problem 02
-# Simple Calculator Program
+# Square the Number Program
 
 def main():
-    pass  # replace this line with all of your code
-    
     print("Square the Number Program")
-    
-    number = int(input("Enter a number: "))
-    
-    print(f"The square of {number} is {(number * number):.2f}")
 
-if __name__ == "__main__":
+    number = int(input("Enter a number: "))
+
+    print(f"The square of {number} is {(number * number):.2f}")
+    
+if _name_ == "_main_":
     main()
